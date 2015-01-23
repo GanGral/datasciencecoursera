ea829f5780c6b38a3a44c77fe9ac45da548d3ca3
unknown <ikorablev@IKORABLEVE6420.osisoft.int>
2015-01-23 18:55:22 +0300

    ProgrammingAssignemnt2_Complete

--- cachematrix.R
+++ cachematrix.R
@@ -3,15 +3,20 @@
 ## Function to store the list of logical functions to cache and inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+## Initially clear the cache
   m <- NULL
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  ##Function to get initial matrix
   get <- function() x
+  
+  ##function to cache inversed matrix
   setinverse <- function(solve) {
     m <<- solve
   }
+  ##function to return inversed matrix
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
@@ -25,10 +30,12 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
+  ##Checks if matrix was already cached, and return it with info message 
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
+  ## assign initial matrix to data, then inverse it with solve, cache it and return
   data <- x$get()
   m <- solve(data, ...)
   x$setinverse(m)
